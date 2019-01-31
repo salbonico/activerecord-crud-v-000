9ceecad40eff414fe2f2a6257b519799491ed014
Steve Albonico <salbonico@gmail.com>
2019-01-31 02:49:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -90,7 +90,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   temp = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  temp.destroy
 end
 
 def can_destroy_all_items_at_once
